7198717a9baf93c67259ba472de59337f3a3b321
Rodion <rodion.arr@gmail.com>
2014-12-08 16:31:54 +0200

    Documentation added

--- class.php
+++ class.php
@@ -33,6 +33,7 @@ class CExcelToOrder extends CBitrixComponent
     /**
      * Функция проверяет наличие на сайте товара с артикулом $artnumber.
      * Второй параметр служит для определения в каком из двух свойств артикула искать: Артикул или Код 1С
+     * 
      * @param $artnumber
      * @param $artnumberType
      * @return bool
@@ -59,6 +60,13 @@ class CExcelToOrder extends CBitrixComponent
 
     /**
      * Получает данные товара:
+     * - общий остаток по складам
+     * - название товара
+     * - сслыку на товар
+     * - ИД товара
+     *
+     * возвращает это все в массиве
+     *
      * @param $artnumber
      * @param $artnumberType
      * @return array|int
@@ -95,23 +103,12 @@ class CExcelToOrder extends CBitrixComponent
     }
 
     /**
-     * @param $dir
+     * Метод проверяет существование склада по имени
+     * Возварщает true если найден и false в противном случае
+     *
+     * @param $storeName
+     * @return bool
      */
-    protected function removeDirectory($dir)
-    {
-        if($dir == '')
-            return false;
-
-        if ($objs = glob($dir."/*"))
-        {
-            foreach($objs as $obj)
-            {
-                is_dir($obj) ? /*$this->removeDirectory($obj)*/ : unlink($obj);
-            }
-        }
-        rmdir($dir);
-    }
-
     protected function checkStore($storeName)
     {
         $arStore = \Epages\CMultiStore::getStorageByName($storeName);
@@ -122,6 +119,13 @@ class CExcelToOrder extends CBitrixComponent
             return false;
     }
 
+    /**
+     * Метод получает количество на указанном по имени складе указанного по ID товара
+     *
+     * @param $productID
+     * @param $storeName
+     * @return int
+     */
     protected function getQuantityOnStore($productID, $storeName)
     {
         $arStores = \Epages\CMultiStore::getStoreAmount($productID);
@@ -138,17 +142,27 @@ class CExcelToOrder extends CBitrixComponent
             return 0;
     }
 
+    /**
+     * Метод принимает файл .xslx из $_POST и возвращает его данные в виде PHP массива
+     *
+     * @param $FILES
+     * @return array
+     */
     protected function parseXslx($FILES)
     {
+        //проверить что файл типа .xslx
         if($FILES["userfile"]["type"] !== "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
             return Array("ERROR_MASSAGES" => GetMessage("ERROR_WRONG_FILE_TYPE"));
         else
         {
+            //создать уникальну для каждого пользователя папку
             if(!is_dir($this->uploadDir))
                 mkdir($this->uploadDir);
 
+            //сохранить файл в папку на сервере
             if (move_uploaded_file($this->fileName, $this->uploadFile))
             {
+                //распаковать .xslx
                 $zip = new ZipArchive;
                 $zip->open($this->uploadFile);
                 $zip->extractTo($this->uploadDir.'/extract');
@@ -161,6 +175,7 @@ class CExcelToOrder extends CBitrixComponent
                     $sharedStringsArr[] = (string)$item->t;
                 }
 
+                //читаем данные книги
                 $handle = @opendir($this->uploadDir.'/extract/xl/worksheets');
                 $out = array();
                 while ($file = @readdir($handle))
@@ -195,29 +210,46 @@ class CExcelToOrder extends CBitrixComponent
         }
     }
 
+    /**
+     * Метод выполняет обраотку файла из $_POST, возвращает массив с данными о товарах из файла для шаблона
+     *
+     * @param $FILES
+     * @return array
+     */
     public function processPostFile($FILES)
     {
+        //разпарсить файл
         $arResult = $this->parseXslx($FILES);
 
         if(array_key_exists("ITEMS", $arResult))
         {
+            //собрать массив для построения таблицы в шаблоне
             foreach($arResult["ITEMS"] as &$arProduct)
             {
                 $productArtnumber = $arProduct[0];
                 $productStoreName = $arProduct[2];
+
+                //проверить существование товара и по артикулу и склад по названию
                 if(
                     $this->checkItemByArtnumber($productArtnumber, $_POST["artnumber_type"])
                     && $this->checkStore($productStoreName)
                 )
                 {
+                    //получить инфо о товаре
                     $arProductData = $this->getProductData($productArtnumber, $_POST["artnumber_type"]);
+
+                    //пометить товар как найден
                     $arProduct["FOUND"] = true;
+
+                    //получить остаток товара на складе
                     $arProduct["CATALOG_QUANTITY"] = $this->getQuantityOnStore($arProductData["ID"], $productStoreName);
+
+                    //название, ссылка, склад товара
                     $arProduct["NAME"] = $arProductData["NAME"];
                     $arProduct["LINK"] = $arProductData["LINK"];
                     $arProduct["STORE_NAME"] = $productStoreName;
                 }
-                else
+                else //пометить товар как ненайден в противном случае
                     $arProduct["FOUND"] = false;
             }
         }
@@ -225,18 +257,25 @@ class CExcelToOrder extends CBitrixComponent
         return $arResult;
     }
 
+    /**
+     * Метод вызывается при отправки формы для добавления товаров из Excel в корзину
+     * @param $arPost
+     */
     public function doProcessOrder($arPost)
     {
-        $arResult = Array();
+        $arResultTmp = Array();
 
+        //определяем в каком свойстве артикула нужно проверят значение
         $artnumberProp = "";
         if($_POST["artnumber_type"] == "arnumber")
             $artnumberProp = "PROPERTY_ARTICUL_PROIZVODITELYA";
         elseif($_POST["artnumber_type"] = "1с-arnumber")
             $artnumberProp = "PROPERTY_CML2_ARTICLE";
 
+        //цикл по товарам, пришедшим из формы
         foreach($arPost["artnumber"] as $key => $artnumber)
         {
+            //получить данные товара по свойству артикула
             $arSelect = Array("ID", "NAME", "CATALOG_QUANTITY");
             $arFilter = Array("IBLOCK_ID"=>Array(SKU_TIRES_IBLOCK_ID, SKU_DISKS_IBLOCK_ID), "ACTIVE"=>"Y", "$artnumberProp" => $artnumber);
             $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
@@ -244,17 +283,25 @@ class CExcelToOrder extends CBitrixComponent
             {
                 $arProductFields = $ob->GetFields();
 
+                //получение количества из формы
                 $quantity = intval($arPost["quantity"][$key]);
 
+                //и максимальное количество на складе по названию склада ИД товара
                 $storeName = $arPost["store"][$key];
                 $storeQuantity = $this->getQuantityOnStore($arProductFields["ID"], $storeName);
 
+                //защита от дурака, проверка данных по количеству
                 if($quantity < 1)
                     $quantity = 1;
+
+                //нельзя положить в корзину товаров больше, чем есть на складе
                 if($quantity > $storeQuantity)
                     $quantity = $storeQuantity;
 
+                //получить данные склада
                 $arStoreInfo = \Epages\CMultiStore::getStorageByName($storeName);
+
+                //собрать данные по товарам и складам в одном массиве
                 $arResultTmp[$arProductFields["ID"]][] = Array(
                     "NAME" => $arProductFields["NAME"],
                     "STORE" => Array(
@@ -269,6 +316,7 @@ class CExcelToOrder extends CBitrixComponent
 
         if(count($arResultTmp) > 0)
         {
+            //преобразовать массив с товарами и скаладами для отправки в конструктор Epages\CMultiStore()
             foreach($arResultTmp as $productId => $arProducts)
             {
                 $arStores = Array();
